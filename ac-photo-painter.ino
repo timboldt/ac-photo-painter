3742783b2fd5b2844d69990745f726b24e55b8c0
Tim Boldt <tim.boldt@gmail.com>
2025-03-29 14:05:29 -0700

    Added low power handling and proper loop logic

--- ac-photo-painter.ino
+++ ac-photo-painter.ino
@@ -4,37 +4,6 @@
 
 PCF85063A rtc(Wire1);
 
-// Original C code behavior:
-//
-// init:
-//   stdio logging
-//   epd spi
-//   sd spi
-//   rtc i2c
-//   VBAT ADC on pin 29
-//   gpio init:
-//     4x epd pins
-//     2x led pins
-//     3x charge/battery pins
-//     2x power control pins
-// watchdog enable
-// sleep 1s
-// rtc init
-// rtc set alarm
-// enable charge state IRQ callback
-// if battery is low:
-//   disable alarm
-//   flash low power led
-//   turn power off
-// led on
-// read SD card
-// if no main power
-//    run display
-//    turn power off
-// in a loop:
-//    wait for key press
-//    run display
-
 const uint32_t CHARGE_STATE = 17;  // Battery charging indicator (low is charging; high is not charging).
 const uint32_t BAT_ENABLE = 18;    // Battery power control (high is enabled; low turns off the power).
 const uint32_t USER_BUTTON = 19;   // User button (low is button pressed, or the auto-switch is enabled).
@@ -43,15 +12,21 @@ const uint32_t VBUS_STATE = 24;    // USB bus power (high means there is power).
 const uint32_t RED_LED = 25;       // Activity LED: red.
 const uint32_t GREEN_LED = 26;     // Power LED: green.
 
+static void do_period_housekeeping(void);
 static void set_time(void);
 static void set_alarm_time(int seconds);
+static float get_battery_voltage(void);
+static void dump_rtc_regs(void);
 
 void setup() {
     //
     // Set up GPIO pins.
     //
 
+    // This ensures that we have the full 4096 ADC resolution.
     analogReadResolution(12);
+    // Read the analog pin once to prime it.
+    get_battery_voltage();
 
     pinMode(CHARGE_STATE, INPUT_PULLUP);
     pinMode(USER_BUTTON, INPUT_PULLUP);
@@ -88,63 +63,83 @@ void setup() {
 }
 
 void loop() {
-    watchdog_update();
+    do_period_housekeeping();
 
-    // uint8_t data[18];
-    // rtc.reg_r(0, data, 18);
-    // for (int i = 0; i < 18; i++) {
-    //     Serial.printf("%02x: %02x\n", i, data[i]);
-    // }
-
-    time_t current_time = rtc.time(NULL);
-    Serial.print("time : ");
-    Serial.print(current_time);
-    Serial.print(", ");
-    Serial.println(ctime(&current_time));
-
-    float vsys = analogRead(A3) * 3.3f / 4096.0f * 3.0f;
-    Serial.println(vsys);
-
-    if (digitalRead(CHARGE_STATE) == LOW) {
-        // Battery is charging.
-        Serial.println("Battery is charging.");
-    } else {
-        // Battery is not charging.
-        Serial.println("Battery is not charging.");
-    }
-    if (digitalRead(USER_BUTTON) == LOW) {
-        // Button is pressed.
-        Serial.println("Button is pressed.");
-    } else {
-        // Button is not pressed.
-        Serial.println("Button is not pressed.");
-    }
-    if (digitalRead(VBUS_STATE) == HIGH) {
-        // USB bus power is present.
-        Serial.println("USB bus power is present.");
-    } else {
-        // USB bus power is not present.
-        Serial.println("USB bus power is not present.");
-    }
-    if (digitalRead(PWR_MODE) == HIGH) {
-        // Power mode is high.
-        Serial.println("Power mode is high.");
-    } else {
-        // Power mode is low.
-        Serial.println("Power mode is low.");
-    }
-
-    Serial.println("Green LED on, red LED off");
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(GREEN_LED, HIGH);
-    delay(1000);
-    Serial.println("Green LED off, red LED on");
+    // Turn on activity indicator.
     digitalWrite(RED_LED, HIGH);
-    digitalWrite(GREEN_LED, LOW);
-    delay(1000);
+    Serial.println("BEGIN WORK");
+
+    delay(2000);
+
+    // if (digitalRead(VBUS_STATE) == HIGH) {
+    //     // USB bus power is present.
+    //     Serial.println("USB bus power is present.");
+    // } else {
+    //     // USB bus power is not present.
+    //     Serial.println("USB bus power is not present.");
+    // }
+    // if (digitalRead(PWR_MODE) == HIGH) {
+    //     // Power mode is high.
+    //     Serial.println("Power mode is high.");
+    // } else {
+    //     // Power mode is low.
+    //     Serial.println("Power mode is low.");
+    // }
 
     // Turn off the battery power.
     digitalWrite(BAT_ENABLE, LOW);
+    // This line will never be reached when running on battery power.
+
+    // Turn off activity indicator.
+    Serial.println("END WORK");
+    digitalWrite(RED_LED, LOW);
+
+    // Wait for user to press button.
+    bool button_pressed = false;
+    while (!button_pressed) {
+        do_period_housekeeping();
+        for (int i = 0; i < 10; i++) {
+            if (!digitalRead(USER_BUTTON)) {
+                Serial.println("Button press detected!");
+                button_pressed = true;
+                break;
+            }
+            delay(100);
+        }
+    }
+}
+
+// Do periodic tasks.
+static void do_period_housekeeping(void) {
+    // Feed the watchdog, so it doesn't bite us.
+    watchdog_update();
+
+    // If the battery is low, disable the alarm, alert the user, and power off.
+    if (get_battery_voltage() < 3.1f) {
+        // Disable alarm.
+        set_alarm_time(-1);
+
+        // Flash 5 times to indicate a problem.
+        for (int i = 0; i < 5; i++) {
+            digitalWrite(RED_LED, HIGH);
+            delay(300);
+            digitalWrite(RED_LED, LOW);
+            delay(300);
+        }
+
+        // Turn off the battery power.
+        digitalWrite(BAT_ENABLE, LOW);
+        // This line will never be reached when running on battery power.
+    }
+
+    // Turn on the green LED if the battery is charging.
+    if (digitalRead(CHARGE_STATE) == LOW) {
+        // Battery is charging.
+        digitalWrite(GREEN_LED, HIGH);
+    } else {
+        // Battery is not charging.
+        digitalWrite(GREEN_LED, LOW);
+    }
 }
 
 // This is useful to get the RTC time set to a known value.
@@ -160,18 +155,48 @@ static void set_time(void) {
 }
 
 // Set an alarm for some time in the future.
+//
 // Maximum time is 24 hours from now.
+// A negative value disables all alarms.
+//
+// Note: 0x80 is used to disable a field.
 static void set_alarm_time(int seconds) {
-    time_t tt = rtc.time(NULL) + seconds;
-    struct tm* t = localtime(&tt);
+    if (seconds >= 0) {
+        time_t tt = rtc.time(NULL) + seconds;
+        struct tm* t = localtime(&tt);
+        rtc.alarm(PCF85063A::HOUR, t->tm_hour);
+        rtc.alarm(PCF85063A::MINUTE, t->tm_min);
+        rtc.alarm(PCF85063A::SECOND, t->tm_sec);
+    } else {
+        // Disable all alarms.
+        rtc.alarm(PCF85063A::HOUR, 0x80);
+        rtc.alarm(PCF85063A::MINUTE, 0x80);
+        rtc.alarm(PCF85063A::SECOND, 0x80);
+    }
 
-    // Note: 0x80 is used to disable a field.
+    // We don't use date or day of week.
     rtc.alarm(PCF85063A::WEEKDAY, 0x80);
     rtc.alarm(PCF85063A::DAY, 0x80);
-    rtc.alarm(PCF85063A::HOUR, t->tm_hour);
-    rtc.alarm(PCF85063A::MINUTE, t->tm_min);
-    rtc.alarm(PCF85063A::SECOND, t->tm_sec);
 
     // Also, clear any pending alarm.
     rtc.int_clear();
+}
+
+// Calculate VSYS, assuming a 12-bit ADC, 3.3Vref, and 3x voltage divider.
+// When on battery, VBAT == VSYS.
+static float get_battery_voltage(void) {
+    float vsys = analogRead(A3) * 3.3f / 4096.0f * 3.0f;
+    // Serial.printf("VBAT: %f V", vsys);
+    // Serial.println();
+    return vsys;
+}
+
+// Dump the I2C registers of the RTC.
+static void dump_rtc_regs(void) {
+    uint8_t data[16];
+    rtc.reg_r(0, data, 16);
+    for (int i = 0; i < 16; i++) {
+        Serial.printf("%02x: %02x", i, data[i]);
+        Serial.println();
+    }
 }
